56c5a003b9e30248f79b3a72f8e864a79ec91f38
Renaud BERNARD <renaud.bernard@p59production.com>
2018-01-22 11:56:27 +0100

    Supr commentary
    Suppr useless commentaries.

--- js/main.js
+++ js/main.js
@@ -6,19 +6,6 @@ MENU 2 --> affiche le contenu 2 et ajoute la bordure sur Menu 2
 MENU 3 --> affiche le contenu 3 et ajoute la bordure sur Menu 3
 
 */
-/*
-$('#tab1').on('click', function() {
-	$('ul li.active').css("border", "solide 2px black");
-	$('ul li:nth-of-type(2)').css("border", "none");
-	$('ul li:nth-of-type(3)').css("border", "none");
-	$('#tab1').show();
-	$('#tab2').hide();
-	$('#tab3').hide();
-
-	this.addClass('active');
-	this.removeClass('active');
-}
-*/
 
 var nav1 = $('ul li:nth-child(1)')
 var nav2 = $('ul li:nth-child(2)')
